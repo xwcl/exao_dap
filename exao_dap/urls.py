239de98aa9561016b51083007f35f097d15b9031
Joseph Long <github@joseph-long.com>
2021-02-09 23:17:44 -0700

    Add about page skeleton

--- exao_dap/urls.py
+++ exao_dap/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('', include('social_django.urls', namespace='social')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
