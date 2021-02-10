05a3f0257530b19f7ff988af91814616f57689ca
Joseph Long <github@joseph-long.com>
2021-02-09 23:17:57 -0700

    WIP models

--- exao_dap/registry/models.py
+++ exao_dap/registry/models.py
@@ -1,9 +1,11 @@
 import uuid
 from django.db import models
+from django.conf import settings
 
 class DataSet(models.Model):
     # storage
     collection_path = models.CharField(primary_key=True, editable=False) # fyi iRODS collections can have really long names
+    source_path = models.CharField(editable=False)
 
     # meta
     class DataSetSource(models.TextChoices):
@@ -13,17 +15,21 @@ class DataSet(models.Model):
         POST = 'post', 'postprocessing'
     source = models.CharField(choices=DataSetSource.choices)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    class State(models.TextChoices):
+        NEW = 'new', 'new'
+        SYNCING = 'syncing', 'syncing'
+        SYNCED = 'synced', 'synced'
+    state = models.CharField(choices=State.choices, default=State.NEW)
     # ways DataSets get made
     # grid = models.ForeignKey('undertaker.Grid', null=True)
     # bigjob = models.ForeignKey('undertaker.BigJob', null=True)
     # source_path = models.CharField
 
     # access control
-    # owner = user
-    # shared = models.ManyToManyField  # can be refreshed when source_path is avail
-    # dependencies = models.ManyToManyField(DataSet)  # for postprocessing products, obtainable through recursive walking of grids and bigjobs but as these should be R/O it is safe to denormalize
-    public = models.BooleanField
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    shared = models.ManyToManyField(settings.AUTH_USER_MODEL)  # can be refreshed on demand from iRODS, should be done nightly or something
+    dependencies = models.ManyToManyField(DataSet)  # for postprocessing products, obtainable through recursive walking of grids and bigjobs but as these should be R/O it is safe to denormalize
+    public = models.BooleanField()
 
 
 class Datum(models.Model):
@@ -36,5 +42,7 @@ class Datum(models.Model):
         INTERMEDIATE = 'intermediate', 'intermediate postprocessing product'
         PRODUCT = 'product', 'high level product'
     kind = models.CharField(choices=DatumKind.choices)
+    imported_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()  # this should be inferred from metadata when available, otherwise use imported_at
     metadata = models.JSONField()
 
